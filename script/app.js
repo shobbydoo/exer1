d74bd49c479bd637cf6a3882672f268366b6dd92
shobbydoo <133627102+shobbydoo@users.noreply.github.com>
2023-09-08 09:54:58 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
+ <script>
+    function displaySum() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('sumButton').addEventListener("click", displaySum);
+    
+  </script>
