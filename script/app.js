f23dc1f6187dc09f5e3af65251ba93a51b426385
shobbydoo <133627102+shobbydoo@users.noreply.github.com>
2023-09-08 10:16:15 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,10 +1,9 @@
- <script>
-    function displaySum() {
+ function displaySum() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("sumanswer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
@@ -19,4 +18,3 @@ document.getElementById('sumButton').addEventListener("click", displaySum);
 
 document.getElementById('diffButton').addEventListener("click", displayDiff);
     
-  </script>
