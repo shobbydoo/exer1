b01247d9cdd37a2031862288e56d44c4a85e3564
shobbydoo <133627102+shobbydoo@users.noreply.github.com>
2023-09-08 10:25:52 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
  function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let sumfirstNum = Number(document.getElementById('firstNum').innerHTML)
+  let sumsecondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum + secondNum;
   document.getElementById("sumanswer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
@@ -9,8 +9,8 @@
 document.getElementById('sumButton').addEventListener("click", displaySum);
 
  function displayDiff() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let difffirstNum = Number(document.getElementById('firstNum').innerHTML)
+  let diffsecondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum - secondNum;
   document.getElementById("diffanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
